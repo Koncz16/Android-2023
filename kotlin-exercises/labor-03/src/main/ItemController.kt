1a38a6f491b9ad5ad5f1f7caa93d80dd25dabe5f
Koncz16 <koncz.hunor@yahoo.com>
2023-10-24 23:38:26 +0300

    Labor 3 - finished

--- kotlin-exercises/labor-03/src/main/ItemController.kt
+++ kotlin-exercises/labor-03/src/main/ItemController.kt
@@ -10,19 +10,25 @@ class ItemController(private val itemService: ItemService) {
         if (items.isEmpty()) {
             println("Nem található elegendő elem a quiz indításához.")
         } else {
+            var correct=0
             println("A quiz elemei:")
             for ((index, item) in items.withIndex()) {
                 println("Kérdés ${index + 1}: ${item.question}")
                 println("Válaszlehetőségek: ${item.answers.joinToString(", ")}")
                 print("Válasz: ")
-                val answer = scanner.nextLine()
-                if (answer == item.answers[item.correct - 1]) {
+                val answer = scanner.nextLine().toInt()
+                if (answer == item.correct) {
                     println("Helyes válasz!")
+                    correct++
                 } else {
                     println("Nem helyes válasz.")
+                    println("A helyes válasz:  ${item.correct}")
                 }
                 println()
             }
+            val percentage = (correct.toDouble() / numberOfItems) * 100
+            println("A teszt véget ért. Összesen $correct helyes válasz volt $numberOfItems kérdésből, ami $percentage%-os eredményt jelent.")
+
         }
     }
 }
--- kotlin-exercises/labor-03/src/main/Main.kt
+++ kotlin-exercises/labor-03/src/main/Main.kt
@@ -1,10 +1,14 @@
 package main
+import java.util.*
+
+private val scanner = Scanner(System.`in`)
 
 fun main(args: Array<String>) {
     val repo=ItemRepository()
     val service=ItemService(repo)
     val itemController=ItemController(service)
-    val numberOfItems = 5
+    print("Adja meg, hogy hány kérdéses legyen a teszt: ")
+    val numberOfItems = scanner.nextLine().toInt()
     itemController.quiz(numberOfItems)
 
 }
